721dac10d5d1fca61dc4d7c3c0f8f958f5e04bdb
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-08-10 04:41:29 -0300

    feat/adicionado migration worker por completo agora

---
+++ src/migration_worker/ExitCodes.ts
@@ -0,0 +1,9 @@
+
+
+enum ExitCodes {
+  SUCCESS = 0,
+  ERROR = 1,
+  STOPPED = 2
+}
+
+export default ExitCodes;
---
+++ src/migration_worker/MessageKind.ts
@@ -0,0 +1,14 @@
+
+enum MessageKind {
+  START = 'START',
+  RESTART = 'RESTART',
+  STOP = 'STOP',
+  PAUSE = 'PAUSE'
+}
+
+// This other thing defines the type of the data that will be sent to the worker thread
+export type MessageData = {
+  reason: string;
+};
+
+export default MessageKind;
