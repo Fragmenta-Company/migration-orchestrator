409c15b0787fb12b906d7bafb044bb336b29f998
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-08-10 04:42:24 -0300

    feat/updated main.ts file to support the rest

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import SqlLoader from "./sql_loader.js";
 import MigrationParser from "./migration_parser.js";
 import MigrationRunner from "./migration_runner.js";
 import "dotenv/config";
+import createDependencyGraph from "./utils/createDependencyGraph.js";
+import { organizeMigrations } from "./utils/organizeMigrations.js";
+import chalk from "chalk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,16 +15,32 @@ const __dirname = path.dirname(__filename);
 const files = await getSqlFilePaths(path.join(__dirname + "/../src-sql"));
 const loader = await SqlLoader.create(files.map(f => path.join(f.parentPath, f.name)));
 
-const migrations = await MigrationParser.parseMultiple(loader.sqlFiles);
+const migrations = await (new MigrationParser).parseMultipleFiles(loader.sqlFiles);
 
-const migration_runner = new MigrationRunner(process.env.DATABASE_URL!);
+const dependencyGraph = createDependencyGraph(migrations);
 
-const success = await migration_runner.runMigrations(migrations);
+const organizedMigrations = organizeMigrations(dependencyGraph, migrations);
 
-if (success) {
-  console.log("All migrations ran successfully.");
+// console.dir(migrations, { depth: null });
+//
+// console.dir(dependencyGraph, { depth: null });
+//
+// console.dir(organizedMigrations, { depth: 2 });
+
+const migration_runner = new MigrationRunner(true);
+
+if (organizedMigrations.success) {
+  const success = await migration_runner.runMigrations(organizedMigrations.migrations);
+
+  if (success) {
+    console.log("All migrations ran successfully.");
+  } else {
+    console.error("Some migrations failed. Please check the logs for details.");
+  }
+
+  await migration_runner.close();
 } else {
-  console.error("Some migrations failed. Please check the logs for details.");
+  organizedMigrations.errors.forEach(error => {
+    console.error(chalk.red(`Error organizing migrations: `), `\n${error.message} at line ${error.line}`);
+  });
 }
-
-await migration_runner.close();
