22a49fbcfff275bddded2fed18ecd0e236d0ff94
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-08-17 03:56:45 -0300

    refact/added indentation to test sql

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,30 @@ async fn main() {
             -- +depends: ::path/to/something.sql::Migration(my_migration)
             -- +tags: concurrent
             -- +nuclear
-            CREATE TABLE IF NOT EXISTS my_table (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-            );
+                CREATE TABLE IF NOT EXISTS my_table (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                );
             -- +rollback
-            DROP TABLE IF EXISTS my_table;
+                DROP TABLE IF EXISTS my_table;
             -- +endmigration
             
             -- +macro: my_macro
             -- +parameters: table_name, type_of_id, ...columns
             -- +depends: ::path/to/something.sql::Migration(my_migration)
-            CREATE TABLE IF NOT EXISTS {{table_name}} (
-                id {{type_of_id}} PRIMARY KEY,
-                {{...columns}},
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-            );
+                CREATE TABLE IF NOT EXISTS {{table_name}} (
+                    id {{type_of_id}} PRIMARY KEY,
+                    {{...columns}},
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                );
             -- +endmacro
 
             -- +migration: another_migration
             -- +depends: ::path/to/something.sql::Migration(my_migration)
-            -- +call: ::/path/to/::my_macro('another_table', 'UUID', 'name VARCHAR(255) NOT NULL', 'age INT')
+                -- +call: ::/path/to/::my_macro('another_table', 'UUID', 'name VARCHAR(255) NOT NULL', 'age INT')
             -- +endmigration
         "#,
     );
